e51896cac4370936f443cf5d43ad325c89811d09
vinu1499 <47626255+vinu1499@users.noreply.github.com>
2019-05-22 10:26:28 +0530

    Update perfect squre or not .py

--- perfect squre or not .py
+++ perfect squre or not .py
@@ -1,3 +1,5 @@
+# perfect squre or not
+
 import math
 num=int(input("enter the num"))
 root=math.sqrt(num)
@@ -5,3 +7,15 @@ if int(root+0.5)**2==num:
     print("the num is a perfect squre")
 else:
     print("the num is not a perfect squre")
+       
+        
+  #perfect squre b/w two intervels
+import math
+num1=int(input("enter the num :"))
+num2=int(input("enter the num :"))
+for i in range(num1,num2,1):
+    root=math.sqrt(i)
+    if int(root+0.5)**2==i:
+         print(i,"the num is a perfect squre")
+    else:
+        print(i,"the num is not a perfect squre")

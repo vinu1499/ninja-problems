8269db1cd8d055ce9549a5652d3c18ea53561097
vinu1499 <47626255+vinu1499@users.noreply.github.com>
2019-06-26 09:57:57 +0530

    Update reverse a number.py

--- reverse a number.py
+++ reverse a number.py
@@ -7,3 +7,17 @@ while(n!=0):
     q=n//10
     n=q
 print(sum)
+
+
+
+
+///another method
+n=int(input())
+sum=0
+
+pow=1
+while(n>=pow):
+    rem=(n//pow)%10
+    sum=rem+sum*10
+    pow=pow*10
+print(sum)
